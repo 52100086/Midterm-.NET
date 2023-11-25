68d9a4b899261e849be206aad00e21036b8f3268
Hieu Pham-origin <hieupham112003@gmail.com>
2023-11-25 11:32:50 +0700

    cuoi cung ko commit nua dau

--- CarRental/Admin/FormChart.cs
+++ CarRental/Admin/FormChart.cs
@@ -44,13 +44,15 @@ namespace CarRental.Admin
             }
 
             double[] revenuesArray = revenues.ToArray(); //
-            double[] datesArray = dates.Select(d => d.ToOADate()).ToArray();
+            double[] datesArray = dates.Select(d => double.Parse(d.ToString("dd"))).ToArray();
 
             // Plot the data
             plt_1.Plot.AddScatter(datesArray, revenuesArray);
             plt_1.Plot.XLabel("Ngày");
             plt_1.Plot.YLabel("Doanh thu");
             plt_1.Plot.Title("Doanh thu theo ngày");
+            plt_1.Plot.XTicks(datesArray);
+
             plt_1.Refresh();
             var groupType = rentals_paid.GroupBy(d => new { d.XeOto.LoaiXe, d.NgayThanhToan });
             var bar = new List<string>();
@@ -73,17 +75,11 @@ namespace CarRental.Admin
                 colors[i] = Color.Blue;  // Màu xanh
             }
 
-            plt_2.Plot.AddBar(revenuesArr, colors[0]);
-            plt_2.Plot.Legend(); // Tạo chú thích mặc định
-
-            //plt_2.Plot.SetLegendLocation(ScottPlot.LegendLocation.Best);
-            // Thêm tất cả các cột vào biểu đồ
-            //barPlot.SetBarLabels(loaixeArr); // Đặt nhãn cho các cột
-
+            var barPlot = plt_2.Plot.AddBar(revenuesArr, colors[0]); // Thêm tất cả các cột vào biểu đồ
+            plt_2.Plot.XTicks(loaixeArr);
             plt_2.Plot.XLabel("Loại xe");
             plt_2.Plot.YLabel("Doanh thu");
             plt_2.Plot.Title("Doanh thu theo loại xe");
-            plt_2.Plot.Legend();  // Hiển thị chú thích
             plt_2.Refresh();
 
         }
